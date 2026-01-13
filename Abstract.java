dda0dfbcf0b4799070b87ba5bb98ff2586f7ac63
Anishka Dhande <anishkadhande8@gmail.com>
2026-01-13 12:43:36 +0530

    Add files via upload

---
+++ Abstract.java
@@ -0,0 +1,20 @@
+abstract class Bike
+{
+ abstract void run();
+
+}
+
+class Honda extends Bike{
+    void run()
+    {
+        System.out.println("running safely");
+    }
+}
+public class Abstract {
+    public static void main(String[] args) {
+        Bike b1=new Honda();
+        b1.run();
+        
+    }
+    
+}
